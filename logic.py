542085d4fa33fa69298638ef6ba0f2b0be60432b
teamsentiorobotics-svg <teamsentiorobotics@gmail.com>
2026-06-26 23:11:47 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,37 @@
+#imprt libraries
+
+
+#pins
+
+
+#functions
+
+
+#main loop
+
+from turtle import left, right
+
+
+speed = 50
+if color = orange:
+    clockwise
+else:
+    anti-clockwise
+speed = 100
+
+while True:
+    if black wall < 320:
+        left_x =  x 
+        left_target = True
+    elif black wall > 320:
+        right_x =  x 
+        right_target = True
+    elif left_x - right_y = 0:
+        drive straight
+    if left_target = True:
+        turn left
+    elif right_target = True:
+        turn right
+    elif right_x  == left_x:
+        drive straight
+#finish
